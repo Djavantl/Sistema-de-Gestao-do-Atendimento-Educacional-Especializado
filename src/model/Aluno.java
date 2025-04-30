852d3a6d8712e91e71e8d0bc48777fce862f7d16
Marley <marleyextreme02@gmail.com>
2025-04-30 18:21:31 -0300

    aluno updated (missing methods)

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,21 +1,51 @@
 package model;
-
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Aluno extends Pessoa{
-	
-    private int matricula;
-    private Deficiencia deficiencia;
-    private List<PlanoAEE> planos = new ArrayList<>();
+public class Aluno extends Pessoa {
+    private String matricula;
+    private String responsavel;
+    private String telResponsavel;
+    private String telTrabalho;
 
-    public Aluno(String nome, String cpf, String endereco, LocalDate dataNascimento, String email, int nmrTelefone, Deficiencia deficiencia ) {
-       super(nome, cpf, endereco, dataNascimento, email, nmrTelefone);
-       this.deficiencia = deficiencia;
+    public Aluno(String nome, LocalDate dataNascimento, String email, String sexo, String naturalidade,
+                 String telefone,String matricula, String telResponsavel, String responsavel, String telTrabalho) {
+        super(nome, dataNascimento, email, sexo, naturalidade, telefone);
+        this.setTelefone(telefone);
+        this.matricula = matricula;
+        this.telResponsavel = telResponsavel;
+        this.responsavel = responsavel;
+        this.telTrabalho = telTrabalho;
     }
-    
-    public void adicionarPlano(PlanoAEE plano){
-        planos.add(plano);
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getTelResponsavel() {
+        return telResponsavel;
+    }
+
+    public void setTelResponsavel(String telResponsavel) {
+        this.telResponsavel = telResponsavel;
+    }
+
+    public String getTelTrabalho() {
+        return telTrabalho;
+    }
+
+    public void setTelTrabalho(String telTrabalho) {
+        this.telTrabalho = telTrabalho;
     }
 }
