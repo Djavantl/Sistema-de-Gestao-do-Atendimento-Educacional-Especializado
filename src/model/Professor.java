28ac6d7ca87fd7213d761d83b4f443f68648d035
Marley <marleyextreme02@gmail.com>
2025-05-02 00:02:25 -0300

    created Professor class

---
+++ src/model/Professor.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Pessoa {
+    private String siape;
+    private String especialidade;
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public Professor(String nome, LocalDate dataNascimento, String email, String sexo, String naturalidade, String telefone, String siape, String especialidade) {
+        super(nome, dataNascimento, email, sexo, naturalidade, telefone);
+        this.siape = siape;
+        this.especialidade = especialidade;
+    }
+
+    public String getSiape() {
+        return siape;
+    }
+
+    public void setSiape(String siape) {
+        this.siape = siape;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public void removerAluno(String matricula) {
+        alunos.removeIf(aluno -> aluno.getMatricula().equals(matricula));
+    }
+
+    public Aluno consultarAlunoPorMatricula(String matricula) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula().equals(matricula)) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+}
