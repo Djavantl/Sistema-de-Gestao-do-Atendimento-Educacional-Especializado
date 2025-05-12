5e25ce185c6e7185898a4d8bbee58ce7717649d1
djavan <104279146+Teixeira10l@users.noreply.github.com>
2025-05-12 15:39:11 -0300

    Refatoração das classes

--- code/src/main/java/org.incluemais/model/dao/RelatorioDAO.java
+++ code/src/main/java/org.incluemais/model/dao/RelatorioDAO.java
@@ -11,32 +11,28 @@ public class RelatorioDAO {
     }
 
     public void inserir(Relatorio r) throws SQLException {
-        String sql = "INSERT INTO Relatorio (titulo, dataGeracao, aluno_matricula, professorAEE_siape, avaliacaoInicial_id, avaliacaoProcessual_id, avaliacaoFinal_id, resumo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO Relatorio (titulo, dataGeracao, aluno_matricula, professorAEE_siape, resumo, observacoes) VALUES (?, ?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, r.getTitulo());
             stmt.setDate(2, Date.valueOf(r.getDataGeracao()));
             stmt.setString(3, r.getAluno().getMatricula());
             stmt.setString(4, r.getProfessorAEE().getSiape());
-            stmt.setInt(5, r.getAvaliacaoInicial().getId());
-            stmt.setInt(6, r.getAvaliacaoProcessual().getId());
-            stmt.setInt(7, r.getAvaliacaoFinal().getId());
-            stmt.setString(8, r.getResumo());
+            stmt.setString(5, r.getResumo());
+            stmt.setString(6, r.getObservacoes());
             stmt.executeUpdate();
         }
     }
 
     public void atualizar(Relatorio r) throws SQLException {
-        String sql = "UPDATE Relatorio SET titulo = ?, dataGeracao = ?, aluno_matricula = ?, professorAEE_siape = ?, avaliacaoInicial_id = ?, avaliacaoProcessual_id = ?, avaliacaoFinal_id = ?, resumo = ? WHERE id = ?";
+        String sql = "UPDATE Relatorio SET titulo = ?, dataGeracao = ?, aluno_matricula = ?, professorAEE_siape = ?, avaliacaoInicial_id = ?, avaliacaoProcessual_id = ?, avaliacaoFinal_id = ?, resumo = ?, observacoes = ?, WHERE id = ?";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, r.getTitulo());
             stmt.setDate(2, Date.valueOf(r.getDataGeracao()));
             stmt.setString(3, r.getAluno().getMatricula());
             stmt.setString(4, r.getProfessorAEE().getSiape());
-            stmt.setInt(5, r.getAvaliacaoInicial().getId());
-            stmt.setInt(6, r.getAvaliacaoProcessual().getId());
-            stmt.setInt(7, r.getAvaliacaoFinal().getId());
-            stmt.setString(8, r.getResumo());
-            stmt.setInt(9, r.getId());
+            stmt.setString(5, r.getResumo());
+            stmt.setString(6, r.getObservacoes());
+            stmt.setInt(7, r.getId());
             stmt.executeUpdate();
         }
     }
